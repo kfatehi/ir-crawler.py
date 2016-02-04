3e27c8aeaa326b9efa2a8b537d31afe6a30bb9c9
Ali <maliaijazuhs12@gmail.com>
2016-02-04 11:26:45 -0800

    Made changes to documentation for FindCommonWords class.

--- src/FindCommonWords.java
+++ src/FindCommonWords.java
@@ -11,11 +11,16 @@ import java.util.HashMap;
 import java.util.Collections;
 
 import java.sql.*;
-
+ 
 public final class FindCommonWords {
 	/**
 	 * Loops through every page's text content to determine word frequencies.
-	 * 
+	 * Initializes a map that will consist of various word frequencies.
+	 * Creates a connection to the database and stores that connection.
+	 * A query is performed to pull in all the text from the pages within a table schema in the
+	 * database. 
+	 * After this query is executed, each line in the text is pulled out and is then tokenized.
+	 * Each word's frequency is recorded and is outputed to a map. It is then sorted and printed out.
 	 * @param args The first element should contain the path to a text file.
 	 */
 	public static void main(String[] args) {
